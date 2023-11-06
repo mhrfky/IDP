17336f6a2a7e86acc72618f3aefb040cd30e39cc
mhrfky <mhrfky@gmail.com>
2023-11-06 02:14:48 +0100

    refactored visualization script, added a calibration stage which just stores the best frame that has the most markers

--- detect_aruco_video.py
+++ detect_aruco_video.py
@@ -1,68 +1,20 @@
-'''
-Sample Command:-
-python detect_aruco_video.py --type DICT_5X5_100 --camera True
-python detect_aruco_video.py --type DICT_5X5_100 --camera False --video test_video.mp4
-'''
-
 import numpy as np
-from utils import ARUCO_DICT, aruco_display
-import argparse
-import time
 import cv2
-import sys
-from visualize_in_2d import visualize_fov
 from visualize_2d_new import visualize_head_pose_from_matrix
-
-from scipy.spatial.transform import Rotation as R
-
 import pandas as pd
 
 
-def load_aruco_dictionary_from_yaml(filename):
-    fs = cv2.FileStorage(filename, cv2.FileStorage_READ)
-    if not fs.isOpened():
-        print(f"Failed to open {filename}")
-        return None
-
-    aruco_dict = cv2.aruco.Dictionary()
-    fn = fs.root()
-    aruco_dict.readDictionary(fn)
-
-    return aruco_dict
-
-
 def setup_detector():
-    global detector
-    # load the ArUCo dictionary, grab the ArUCo parameters, and detect
-    # the markers
-    filename = "my_custom_dictionary.yml"
-    arucoDict = load_aruco_dictionary_from_yaml(filename)
-    arucoParams = cv2.aruco.DetectorParameters()
-    arucoParams.adaptiveThreshConstant = 10
-    arucoParams.adaptiveThreshWinSizeMax = 30
-    arucoParams.adaptiveThreshWinSizeMin = 3
-    arucoParams.adaptiveThreshWinSizeStep = 25
+    aruco_params = cv2.aruco.DetectorParameters()
+    aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_APRILTAG_36h11)
 
-    detector = cv2.aruco.ArucoDetector(arucoDict, arucoParams)
+    detector_ = cv2.aruco.ArucoDetector(aruco_dict, aruco_params)
+
+    return detector_
 
 
-def draw_markers(image, corners):
-    cv2.aruco.drawDetectedMarkers(image, corners)
-
-
-def get_poses(ids, corners):
-    d = []
-    if ids is None:
-        return
-    for id, corner in zip(ids, corners):
-        rvec, tvec, markerPoints = cv2.aruco.estimatePoseSingleMarkers(corner, 0.1341, camera_matrix, dist_matrix)
-        rot_mtx, _ = cv2.Rodrigues(rvec)
-        cameraPose = np.eye(4)
-        cameraPose[:3, :3] = rot_mtx
-        cameraPose[:3, 3] = tvec.squeeze()
-        d.append((id, cameraPose))
-
-    return {i[0][0]: i[1] for i in sorted(d, key=lambda x: x[0])}
+def draw_markers(image, corners_for_drawing):
+    cv2.aruco.drawDetectedMarkers(image, corners_for_drawing)
 
 
 def get_intrinsics():
@@ -74,15 +26,6 @@ def get_intrinsics():
 
 
 def display(image):
-    # Get the dimensions of the image
-    # height, width = image.shape[:2]
-    #
-    # # Define the scale factor
-    # scale_factor = 2.0  # Example: 2.0 will double the display size of the image
-    #
-    # # Resize the image
-    # image = cv2.resize(image, (int(width * scale_factor), int(height * scale_factor)))
-
     cv2.imshow("Image", image)
 
 
@@ -117,7 +60,7 @@ def get_transformation_matrix(curr_poses):
         dst_pts.extend(curr_poses[id])
     src_pts = np.array(src_pts)
     dst_pts = np.array(dst_pts)
-
+    # print(src_pts, dst_pts)
     H, _ = cv2.findHomography(src_pts, dst_pts)
 
     # Extract rotation matrix from the homography
@@ -129,24 +72,7 @@ def get_transformation_matrix(curr_poses):
     return T
 
 
-def kalman_filter_setup():
-    # Initialize Kalman Filter
-    kf = cv2.KalmanFilter(6, 6)  # State size: 6, Measurement size: 6
-
-    kf.transitionMatrix = np.eye(6, dtype=np.float32)  # For example, Identity Matrix for a simple model
-    kf.measurementMatrix = np.eye(6, dtype=np.float32)  # Adjust accordingly
-    kf.processNoiseCov = np.eye(6, dtype=np.float32) * 0.1  # Adjust the noise level accordingly
-    kf.measurementNoiseCov = np.eye(6, dtype=np.float32) * 0.05  # Adjust the noise level accordingly
-    kf.errorCovPost = np.eye(6, dtype=np.float32)  # Posteriori error estimate covariance matrix
-
-    # Initialize statePost, if needed
-    kf.statePost = np.zeros((6, 1), dtype=np.float32)  # Adjust initial state as needed
-
-    return kf
-
-
 def get_gaze_positions():
-
     # Load the DataFrame (assuming you've already done this)
     df = pd.read_csv("000/exports/000/gaze_positions.csv")
 
@@ -155,88 +81,88 @@ def get_gaze_positions():
 
     df_max_confidence = df_max_confidence[['world_index', 'norm_pos_x', 'norm_pos_y']]
 
-    return  df_max_confidence.values.tolist()
+    return df_max_confidence.values.tolist()
+
+
+def add_new_marker_using_homography(init_poses, new_marker_corners, H_current, camera_matrix, new_marker_id):
+
+    # Compute the inverse of the current homography
+    H_current_inv = np.linalg.inv(H_current)
+
+    # Initialize an array to hold the estimated initial positions of the new marker corners
+    estimated_init_corners = np.zeros_like(new_marker_corners)
+
+    # Map each corner point of the new marker using the inverse homography
+    for i, corner in enumerate(new_marker_corners):
+        homogenous_corner = np.append(corner, 1)  # Convert to homogenous coordinates
+        estimated_init_corner = np.dot(H_current_inv, homogenous_corner)
+        # Normalize to get the estimated initial position in Cartesian coordinates
+        estimated_init_corners[i] = estimated_init_corner[:2] / estimated_init_corner[2]
+
+    # Add the estimated initial position of the new marker to init_poses
+    init_poses[new_marker_id] = estimated_init_corners
+
+    return init_poses
 
 
 video = "000/world.mp4"
 video = cv2.VideoCapture(video)
-
+calibrated = False
 # Set the starting frame to 500
-video.set(cv2.CAP_PROP_POS_FRAMES, 0)
+video.set(cv2.CAP_PROP_POS_FRAMES, 1)
 get_intrinsics()
-setup_detector()
+detector = setup_detector()
 
 init_poses = None
-kf = kalman_filter_setup()
 
 fps = video.get(cv2.CAP_PROP_FPS)
 delay = int(1000 / fps)  # Real-time delay
 number_of_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
-weights = [0.5, 0.3, 0.2]  # Example weights for the last three transformations
-measurements = []  # List to hold the last n measurements
-n = 3  # Number of matrices to consider for moving average
-
-# Initialize previous state for Kalman filter
-previous_state = None
-
 gaze_positions = get_gaze_positions()
 gaze_positions_iter = iter(gaze_positions)
+avg_camera_pose = None
 
 while True:
     ret, frame = video.read()
     gaze_position = next(gaze_positions_iter)
-    shape = frame.shape
+
     if not ret:
         break
-
     corners, ids, rejected = detector.detectMarkers(frame)
     if ids is None:
         continue  # Skip the rest of the loop if no markers are detected
     ids = [id[0] for id in ids]
     cornerss = [marker[0] for marker in corners]
 
-    if init_poses is None:
-        init_poses = dict(zip(ids, cornerss))
     curr_poses = dict(zip(ids, cornerss))
+    if calibrated:
+
+        try:
+            avg_camera_pose = get_transformation_matrix(curr_poses)
+        except Exception as e:
+            print(f"No markers is not detected this frame, hence this exception has occured : {e}")
+
+        if avg_camera_pose is  None:
+            continue
+
+        visualize_head_pose_from_matrix(avg_camera_pose, gaze_position, curr_poses)
+
+    elif init_poses is None or (not calibrated and len(curr_poses.keys()) > len(init_poses)):
+        init_poses = curr_poses
 
     draw_markers(frame, corners)
     display(frame)
 
-    avgCameraPose = get_transformation_matrix(curr_poses)
-
-    if avgCameraPose is None:  # Handle the case if you don't get a transformation matrix
-        continue
-
-    # Convert transformation matrix to translation and Euler angles
-    tvec = avgCameraPose[:3, 3]
-    rotation = R.from_matrix(avgCameraPose[:3, :3])
-    roll, pitch, yaw = rotation.as_euler('zyx', degrees=True)
-
-    # Form the state vector [x, y, z, roll, pitch, yaw]
-    state = np.array([tvec[0], tvec[1], tvec[2], roll, pitch, yaw], dtype=np.float32)
-    state = state.reshape(-1, 1)  # Reshape to column vector
-
-    if previous_state is None:
-        kf.statePost = state
-    else:
-        # Use Kalman filter to predict the state
-        predicted_state = kf.predict()
-
-        # Update the Kalman filter with the current measurement
-        kf.correct(state)
-
-    # Store this state for the next iteration
-    previous_state = state
-
-    visualize_head_pose_from_matrix(avgCameraPose, gaze_position,curr_poses)
-
     key = cv2.waitKey(delay) & 0xFF
     if key == ord("q"):
         break
+    elif key == ord("r") and not calibrated:
+        calibrated = True
+
     elif key == ord("w"):
 
-        key =  cv2.waitKey()
+        key = cv2.waitKey()
         if key == ord("q"):
             break
 
--- visualize_2d_new.py
+++ visualize_2d_new.py
@@ -3,111 +3,134 @@ import cv2
 
 from scipy.spatial.transform import Rotation
 
+N = 10  # number of past poses to store and visualize
+
+past_poses = []  # a global variable to keep the previous poses
+WIDTH = 240
+HEIGHT = 160
+VIDEO_WIDTH, VIDEO_HEIGHT = 1280, 720
+IMG_SIZE = (600, 600)
+FONT = cv2.FONT_HERSHEY_SIMPLEX
+FONT_SCALE = 0.5
+TEXT_COLOR = (0, 0, 0)  # Black color
+TEXT_THICKNESS = 1
+TEXT_LINE_SPACING = 20
+FOV_THICKNESS = 2
+FOV_COLOR = (255,0,0)
+MARKER_THICKNESS=2
+MARKER_VISIBLE_COLOR = (0,0,255)
+MARKER_NOT_VISIBLE_COLOR = (0,255,255)
+GAZE_CIRCLE_COLOR = (0,255,0)
+GAZE_CIRCLE_RADIUS = 5
+MARKER_ENLARGEMENT_RATE = 2
+def visualize_head_pose_from_matrix(transformation_matrix, gaze_position, markers=None):
+    global past_poses
+
+    rotation_matrix = decompose_matrix(transformation_matrix)
+    yaw, pitch, roll = rotation_matrix_to_euler_angles(rotation_matrix)
+
+    img = np.ones((IMG_SIZE[1], IMG_SIZE[0], 3), dtype=np.uint8) * 255
+
+    center, center_x, center_y, translated_rect = get_translated_view_rectangle(pitch, roll, yaw)
+
+    draw_gaze_circle(center_x, center_y, gaze_position, img)
+    draw_markers(center, markers, img)
+
+    # Add the current pose to the buffer and remove the oldest if exceeded size
+    visualize_fov(img, translated_rect)
+
+    write_rotations(img, transformation_matrix, pitch, roll, yaw)
+    cv2.imshow('Head Pose', img)
+
 
 def decompose_matrix(matrix):
     rotation = matrix[:3, :3]
     return rotation
 
 
-def rotation_matrix_to_euler_angles(R):
-    # Yaw
-    psi = np.arctan2(R[1, 0], R[0, 0])
-
-    # Pitch
-    theta = np.arctan2(-R[2, 0], np.sqrt(R[2, 1] ** 2 + R[2, 2] ** 2))
-
-    # Roll
-    phi = np.arctan2(R[2, 1], R[2, 2])
-
-    rot = Rotation.from_matrix(R)  # Convert from rotation matrix to Rotation object
+def rotation_matrix_to_euler_angles(rotation_matrix):
+    rot = Rotation.from_matrix(rotation_matrix)  # Convert from rotation matrix to Rotation object
     euler_angles = rot.as_euler('zyx',
                                 degrees=True)  # Convert to Euler angles ('zyx' is one of the possible conventions)
-    print(euler_angles)
     return euler_angles
 
 
-N = 10  # number of past poses to store and visualize
-
-past_poses = []  # a global variable to keep the previous poses
-
-def visualize_head_pose_from_matrix(matrix, gaze_position, curr_poses= None):
-
-    global past_poses
-    R = decompose_matrix(matrix)
-    yaw, pitch, roll = rotation_matrix_to_euler_angles(R)
-
-    width = 120
-    height = 80
-    video_width, video_height = 1280, 720
-    rect_width, rect_height = width, height
-
-    img_size = (600, 600)
-    img = np.ones((img_size[1], img_size[0], 3), dtype=np.uint8) * 255
-
-    center_y = int((90 - yaw) / 180 * img_size[1])
-    center_x = int((90 + pitch) / 180 * img_size[0])
+def get_translated_view_rectangle(pitch, roll, yaw):
+    center_y = int((90 - yaw) / 180 * IMG_SIZE[1])
+    center_x = int((90 + pitch) / 180 * IMG_SIZE[0])
     center = (center_x, center_y)
-
-
     rect = np.array([
-        [-width / 2, -height / 2],
-        [width / 2, -height / 2],
-        [width / 2, height / 2],
-        [-width / 2, height / 2]
+        [-WIDTH / 2, -HEIGHT / 2],
+        [WIDTH / 2, -HEIGHT / 2],
+        [WIDTH / 2, HEIGHT / 2],
+        [-WIDTH / 2, HEIGHT / 2]
     ])
-
     rotation_matrix = cv2.getRotationMatrix2D((0, 0), roll, 1)
     rotated_rect = np.dot(rect, rotation_matrix[:, :2].T)
     translated_rect = rotated_rect + center
+    return center, center_x, center_y, translated_rect
 
-    # Drawing gaze circle inside the rectangle
-    _, norm_pos_x, norm_pos_y = gaze_position
-    gaze_x = center_x - width/2 + norm_pos_x * (width)
-    gaze_y = center_y + height/2 - norm_pos_y * (height)  # inverted Y axis as image coordinates work from top-left
-    cv2.circle(img, (int(gaze_x), int(gaze_y)), 5, (0, 255, 0), -1)  # Green circle with radius of 5
-    for id, marker in curr_poses.items():
-        normalized_marker = [
-            [(point[0] / video_width) * rect_width + center[0] - rect_width / 2,
-             (point[1] / video_height) * rect_height + center[1] - rect_height / 2]
-            for point in marker]
 
-        # Draw the markers inside the rectangle
-        for j in range(4):
-            start_point = (int(normalized_marker[j][0]), int(normalized_marker[j][1]))
-            end_point = (int(normalized_marker[(j + 1) % 4][0]), int(normalized_marker[(j + 1) % 4][1]))
-            cv2.line(img, start_point, end_point, (0, 255, 0), 2)  # Green color for markers
-
-    # Add the current pose to the buffer and remove the oldest if exceeded size
+def visualize_fov(img, translated_rect):
+    global past_poses
     past_poses.append(translated_rect)
     if len(past_poses) > N:
         past_poses.pop(0)
-
     # Draw past poses with decreasing opacity
     for i, previous_rect in enumerate(past_poses[::-1]):
         alpha = (N - i) / N  # fades out the further we go back in history
         for j in range(4):
             start_point = tuple(previous_rect[j].astype(int))
             end_point = tuple(previous_rect[(j + 1) % 4].astype(int))
-            cv2.line(img, start_point, end_point, (255 * alpha, 0, 0), 2)
+            color = [c * alpha for c in FOV_COLOR]
+            cv2.line(img, start_point, end_point, color,FOV_THICKNESS)
 
-    # ... [rest of the function remains unchanged]
 
+def write_rotations(img, matrix, pitch, roll, yaw):
     # Add transformation matrix, roll, pitch, and yaw to the bottom right
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    font_scale = 0.5
-    color = (0, 0, 0)  # Black color
-    thickness = 1
-    line_spacing = 20
-    y0 = img_size[1] - (matrix.shape[0] * line_spacing + 3 * line_spacing)
 
+    y0 = IMG_SIZE[1] - (matrix.shape[0] * TEXT_LINE_SPACING + 3 * TEXT_LINE_SPACING)
     for i, row in enumerate(matrix):
         text = " ".join(["{:.2f}".format(val) for val in row])
-        position = (10, y0 + i * line_spacing)
-        cv2.putText(img, text, position, font, font_scale, color, thickness)
+        position = (10, y0 + i * TEXT_LINE_SPACING)
+        cv2.putText(img, text, position, FONT, FONT_SCALE, TEXT_COLOR, TEXT_THICKNESS)
+    y_pos = y0 + matrix.shape[0] * TEXT_LINE_SPACING
+    cv2.putText(img, f"Yaw: {yaw:.2f}", (10, y_pos), FONT, FONT_SCALE, TEXT_COLOR, TEXT_THICKNESS)
+    cv2.putText(img, f"Pitch: {pitch:.2f}", (10, y_pos + TEXT_LINE_SPACING), FONT, FONT_SCALE, TEXT_COLOR,
+                TEXT_THICKNESS)
+    cv2.putText(img, f"Roll: {roll:.2f}", (10, y_pos + 2 * TEXT_LINE_SPACING), FONT, FONT_SCALE, TEXT_COLOR,
+                TEXT_THICKNESS)
 
-    y_pos = y0 + matrix.shape[0] * line_spacing
-    cv2.putText(img, f"Yaw: {yaw:.2f}", (10, y_pos), font, font_scale, color, thickness)
-    cv2.putText(img, f"Pitch: {pitch:.2f}", (10, y_pos + line_spacing), font, font_scale, color, thickness)
-    cv2.putText(img, f"Roll: {roll:.2f}", (10, y_pos + 2 * line_spacing), font, font_scale, color, thickness)
-    cv2.imshow('Head Pose', img)
 
+def draw_markers(center, markers, img, ):
+    for marker_id, marker in markers.items():
+        # Calculate the center of the marker
+        marker_center = [sum(p[0] for p in marker) / len(marker),
+                         sum(p[1] for p in marker) / len(marker)]
+
+        # Scale the markers by the enlargement rate
+        scaled_marker = [
+            [marker_center[0] + (point[0] - marker_center[0]) * MARKER_ENLARGEMENT_RATE,
+             marker_center[1] + (point[1] - marker_center[1]) * MARKER_ENLARGEMENT_RATE]
+            for point in marker]
+
+        # Normalize the marker points
+        normalized_marker = [
+            [(point[0] / VIDEO_WIDTH) * WIDTH + center[0] - WIDTH / 2,
+             (point[1] / VIDEO_HEIGHT) * HEIGHT + center[1] - HEIGHT / 2]
+            for point in scaled_marker]
+
+        # Draw the markers inside the rectangle
+        for j in range(4):
+            start_point = (int(normalized_marker[j][0]), int(normalized_marker[j][1]))
+            end_point = (int(normalized_marker[(j + 1) % 4][0]), int(normalized_marker[(j + 1) % 4][1]))
+            cv2.line(img, start_point, end_point, MARKER_VISIBLE_COLOR, MARKER_THICKNESS)
+
+
+
+def draw_gaze_circle(center_x, center_y, gaze_position, img):
+    # Drawing gaze circle inside the rectangle
+    _, norm_pos_x, norm_pos_y = gaze_position
+    gaze_x = center_x - WIDTH / 2 + norm_pos_x * WIDTH
+    gaze_y = center_y + HEIGHT / 2 - norm_pos_y * HEIGHT  # inverted Y axis as image coordinates work from top-left
+    cv2.circle(img, (int(gaze_x), int(gaze_y)), GAZE_CIRCLE_RADIUS, GAZE_CIRCLE_COLOR, -1)  # Green circle with radius of 5
